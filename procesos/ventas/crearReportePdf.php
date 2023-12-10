85d87726d828d1832206560b839021661f2fb7f4
CoffeeDuck <davidlive0159@gmail.com>
2023-12-10 13:30:59 -0500

    actualizacion de ruta reportes

--- procesos/ventas/crearReportePdf.php
+++ procesos/ventas/crearReportePdf.php
@@ -18,7 +18,7 @@ function file_get_contents_curl($url) {
     return $data;
 }
 
- $html=file_get_contents("http://localhost/vistas/ventas/reporteVentaPdf.php?idventa=".$id);
+ $html=file_get_contents("http://localhost/proyecto/vistas/ventas/reporteVentaPdf.php?idventa=".$id);
 
 
  
--- procesos/ventas/crearTicketPdf.php
+++ procesos/ventas/crearTicketPdf.php
@@ -18,7 +18,7 @@ function file_get_contents_curl($url) {
     return $data;
 }
 
- $html=file_get_contents("http://localhost/vistas/ventas/ticketVentaPdf.php?idventa=".$id);
+ $html=file_get_contents("http://localhost/proyecto/vistas/ventas/ticketVentaPdf.php?idventa=".$id);
 
 
  
